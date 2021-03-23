2e9972ed6a41857323bfeffc2db6df83f8ae0063
Yuya335 <illsurvive.es.335@gmail.com>
2021-03-23 17:24:26 +0900

    Rooting基礎

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
+// ========================================================||
+// 【sarverプログラム】
+// ========================================================||
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const ejs = require("ejs");
 
+// ページ（テンプレートファイル）の読み込み
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
+const other_page = fs.readFileSync("./other.ejs", "utf-8");
 
+// サーバーの起動
+// 3000番ポートで待ち受け
 let server = http.createServer(getFormClient);
-
 server.listen(3000);
 console.log("Server start!!");
 
 // メインプログラムここまで=======================
 function getFormClient(request, response) {
-  const content = ejs.render(index_page);
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(content);
-  response.end();
+  const url_parts = url.parse(request.url);
+  // コンテンツ格納用変数
+  let content;
+  switch (url_parts.pathname) {
+    case "/":
+      // contentのレンダリング
+      // ejs.render(レンダリングデータ, オブジェクト);
+      content = ejs.render(index_page, {
+        title: "Indexページです。",
+        content: "これはテンプレートを使ったIndexページです。",
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+    case "/other":
+      content = ejs.render(other_page, {
+        title: "Otherページです。",
+        content: "これは追加したOtherページです。",
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+    default:
+      response.writeHead(200, { "content-type": "text/plain" });
+      response.end("no page...");
+  }
 }
